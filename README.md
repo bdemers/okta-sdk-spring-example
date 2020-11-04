6a716f88c547dc4c6f9952885a17a3b8ce6f06e0
Brian Demers <bdemers@apache.org>
2020-11-04 12:19:25 -0500

    Add basic Okta example that lists all users to the console

---
+++ README.md
@@ -0,0 +1,36 @@
+Okta SDK + Spring Boot Example
+==============================
+
+Basic example application that uses the Okta SDK for Java in a Spring Boot application.
+
+To use this project:
+
+1. Clone it:
+   ```sh
+   git clone https://github.com/bdemers/okta-sdk-spring-example.git
+   cd okta-sdk-spring-example
+   ```
+
+2. Configure it:
+
+   Set the following to properties via any [Spring configuration technique](https://docs.spring.io/spring-boot/docs/1.5.6.RELEASE/reference/html/boot-features-external-config.html) 
+   
+   | Key | Description | |
+   ------|----------------
+   | `okta.client.orgUrl` | Your Okta Url: `https://{yourOktaDomain}`, i.e. `https://dev-123456.okta.com` | [More Info](https://bit.ly/finding-okta-domain) |
+   | `okta.client.token` | An Okta API token, see [creating an API token](https://developer.okta.com/docs/api/getting_started/getting_a_token) for more info. | [https://bit.ly/get-okta-api-token]() |
+   
+   For example, one easy way to get started is to add these properties to the `src/main/resources/application.properties` file in this repo:
+
+   ```properties
+   okta.client.orgUrl=https://dev-123456.okta.com
+   okta.client.token=some-super-secret-api-token-here
+   ```
+
+   **NOTE:** Replace `https://dev-123456.okta.com` with your actual [Okta URL](https://bit.ly/finding-okta-domain).
+
+3. Run it:
+
+   ```shell
+   ./mvnw spring-boot:run
+   ```
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,43 @@
 package com.example.demo;
 
+import com.okta.sdk.client.Client;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
+    /**
+     * This component will execute the 'run' method when the application starts. This is meant to demonstrate the usage
+     * of the Okta SDK {@link Client}.  The usage pattern with be the same in a Web application.
+     */
+    @Component
+    static class SimpleCommandLineRunner implements CommandLineRunner {
+
+        // injected automatically via the constructor, you could use @Autowired too
+        private final Client client;
+
+        SimpleCommandLineRunner(Client client) {
+            this.client = client;
+        }
+
+        @Override
+        public void run(String... args) throws Exception {
+
+            // list all of the Okta Users, and print the id
+            client.listUsers().forEach(user ->
+                    System.out.println(user.getProfile().getLogin()));
+
+            // NOTE, this collection will paginate automatically, so if you have a million users you will
+            // be here a while, instead you may want pass a 'filter' into the listUsers method.
+
+            // For more examples see: https://github.com/okta/okta-sdk-java#usage-guide
+        }
+    }
 }
